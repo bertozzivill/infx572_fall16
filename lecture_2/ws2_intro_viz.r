6edae9c8ce9ee0ee4839f418a6edad64718c6cb1
Amelia Bertozzi-Villa <bertozzivill@gmail.com>
2016-09-04 18:26:09 -0700

    Finish ws2

--- lecture_2/ws2_intro_viz.r
+++ lecture_2/ws2_intro_viz.r
@@ -12,8 +12,7 @@
 
 ## II. Visualizations
 ##  1. Plotting in {base}
-##  2. What is ggplot?
-##  3. Basic plotting in ggplot
+##  2. Basic plotting in ggplot
 ##############################################################################
 
 #####################
@@ -72,6 +71,32 @@ summary(data_frame)
 
 ## Interlude: Loading libraries ---------------------------------------------------------
 
+## The standard version of R that you've recently downloaded is great, but it only includes so much stuff. 
+## Over the years, researchers have found themselves thinking "man, I wish the plotting tools were better in R",
+## or "man, I wish there were a bunch of useful datasets that I could easily load into R as examples", or 
+## "man, I wish I could run a nonlinear hierarchical regression with this new optimization algorithm I just 
+## heard about".
+
+## The amazing thing about open-source languages like R is that, when users of the program have feelings like this,
+## They can just write R code that does exactly what they want, and, if they think it will be useful to others, 
+## they can make those tools publicly available. If the folks who maintain R collectively decide that this new
+## *package* (aka *library*) is useful, they'll put it in a shared database so users can install it right from the R console.
+
+## To use a library in R, there are two steps: First, you have to *install* it on your computer. You only have to do this 
+## once. Let's say, for example, that we want to install the library "car", which contains some nice example datasets.
+## The first step would be:
+install.packages("car")
+
+## Ok, now {car} is installed on our computer, but to save memory R doesn't automatically load that functionality into
+## the active environment. In order to make all the functionality of your new library available to you, you need to 
+## explictly ask for it using the library() function. So, to actually be able to use the elements of {car}, you would say:
+library(car)
+
+## and now you're ready to go! Usually, coders put all the librarys they need for a script at the very top of the script,
+## so they're easier to keep track of. 
+
+## We'll use the {car} library in the next section. Now it's your turn: can you install and load the package 
+## {ggplot2} into this environment?
 
 ## 3. Loading data ---------------------------------------------------------
 
@@ -101,71 +126,101 @@ head(Salaries)
 
 ## Let's use this dataset to practice some simple plots.
 
+#########################
+## II. Plotting in R  
+########################
 
+## 1. Plotting in {base} ---------------------------------------------------------
 
-###---- below is the code from my guest lecture of Mike's class, to be poached as needed for this lecture
+## The standard R that exists before you load in any libraries (which we denote by {base})
+## has plotting functionality. Let's see what happens if we just throw our Salaries dataset
+## at it.
+plot(Salaries)
+
+## Whoa! That did... something. What did that do?
+
+## What if we only select two columns?
+plot(Salaries[c("rank", "sex")])
+plot(Salaries[c("sex", "yrs.since.phd")])
+plot(Salaries[c("yrs.since.phd", "salary")])
+
+## Ok, that's fine, it has some decent default functionality. But frankly it's ugly, it's unweildy,
+## and we will never speak of it again. We'll move on to bigger and better things in the form of....
+
+##     **
+##    ****
+##   ******
+##  ********
+## **********
+
+## **ggplot2**
+
+## **********
+##  ********
+##   ******
+##    ****
+##     **
+
+## 2. Plotting in ggplot ---------------------------------------------------------
+
+## ggplot (or, more specifically, its updated version ggplot2) is an extremely popular plotting library 
+## for R. It's more involved than the simple plot() function we demo'd above, but it's also much more 
+## versatile. We're just going to cover the basics now, but its power will become clear to you shortly. 
+
+## If you didn't already load ggplot2 into your R environment, let's do that now.
 library(ggplot2)
-library(data.table)
-library(reshape2)
-library(dplyr)
 
-load("suicides.rdata")
-all_suicides <- copy(suicides)
-suicides <- suicides %>% 
-  group_by(year, state, means) %>% 
-  mutate(deaths = sum(deaths))
+## Let's start by just looking at the most basic function in ggplot: ggplot() 
+bare <- ggplot(Salaries)
 
-# start from the very basics: what is ggplot, alone?
-bare <- ggplot(suicides)
+## That did... nothing? 
+## Not quite: it created an object called "bare" that, when you call it, gives us a blank grey slate.
+## If you look at the *attributes* of this object, you'll see that it is indeed a lot of blank spaces
+## waiting to be filled.
 
-# what are plot aesthetics?
-aesthetic <- ggplot(suicides, aes(x=year, y=deaths))
+## Next step: what are plot aesthetics?
+aesthetic <- ggplot(Salaries, aes(x=yrs.since.phd, y=salary))
 
-# how do you put actual stuff on the plot?
-scatter <- ggplot(suicides, aes(x=year, y=deaths)) +
+## Ok, adding "aes" (the plot *aesthetics*) is still mostly blank but gives us a little more information:
+## it's populated an x and y axis onto the plot. 
+## Broadly, an *aesthetic* is anything that maps data in the dataset onto some element of the plot (x axis location,
+## y axis location, color, grouping, etc.) We'll get more into this later. 
+
+## Next: how do you put actual stuff on the plot?
+## You have to add another function on to the end, saying what type of plot you want to make. 
+## geom_point(), for example, will yield a scatterplot:
+scatter <- ggplot(Salaries, aes(x=yrs.since.phd, y=salary)) +
   geom_point()
 
-# split "means" out by color
-color_by_means <- ggplot(suicides, aes(x=year, y=deaths, color=means)) +
-  geom_point(size=3)
+## Success! We have ourselves our first actual plot. To understand what geom_point() did, 
+## let's take a look at the "layers" attribute of our three ggplot objects "bare", "aesthetic", and "scatter":
+bare$layers
+aesthetic$layers
+scatter$layers
 
-# facet out by state, toggle scales on and off
-scatter_by_state <- ggplot(suicides, aes(x=year, y=deaths, color=means)) +
-  geom_point(size=3) +
-  facet_wrap(~state, scales="free")
+## QUIZ: what did geom_point() do?
 
-# experiment with a different type of plot
-line_by_state <- ggplot(suicides, aes(x=year, y=deaths, color=means)) +
-  geom_line(size=3) +
-  facet_wrap(~state, scales="free")
+## Ok, this scatter plot has been great fun, but can we make different types of plot? 
 
-# experiment with a third type of plot. Also: plotting functions have aesthetics too!
-bar_by_state <- ggplot(suicides, aes(x=year, y=deaths, color=means)) +
-  geom_bar(aes(fill=means), stat="identity") +
-  facet_wrap(~state, scales="free")
+## LINE PLOT: geom_line:
+line <- ggplot(Salaries, aes(x=yrs.since.phd, y=salary)) +
+        geom_line()
 
-##------------------------------------------------------------------
-## look at a one state example to explor other dimensions/plot formats
-##------------------------------------------------------------------
-one_state <- all_suicides[all_suicides$state=="Haryana"] %>% 
-  group_by(year, state, sex, age, means) %>% 
-  mutate(deaths = sum(deaths))
+## that looks weird! Which should make you think: what is the appropriate plot type to use here?
 
+## BAR PLOT: geom_bar() (NOTE: can't have a y-value aesthetic for this one)
+bar <- ggplot(Salaries, aes(x=yrs.since.phd)) +
+          geom_bar()
 
-#multiple aes values per plotting function, facet_grid, labels
-density_plots <- ggplot(one_state, aes(x=deaths)) +
-  geom_density(aes(color=means, fill=means), size=1, alpha=0.5) +
-  facet_grid(age~sex, scales="free") +
-  labs(title="Distribution of Suicides by Age, Sex, and Means, 2001-2010",
-       x="Deaths",
-       y="Density")
+## That's it for our intro plotting lesson. I know it was boring and not very colorful, so to prove to you 
+## how cool ggplot is let me leave you with this, which you can create in six lines of code:
 
-one_state <- all_suicides[all_suicides$state=="Haryana"] %>% 
-  group_by(year, state, means) %>% 
-  mutate(deaths = sum(deaths))
+fancy <- ggplot(Salaries, aes(x=yrs.since.phd, y=salary)) +
+              geom_point(aes(color=rank), alpha=0.6) +
+              facet_grid(sex~discipline) +
+              labs(title="Salary by Rank, Discipline, and Sex in an American University",
+                   x="Years Since Ph.D",
+                   y="Salary (USD)")
 
-#multiple plotting functions per plot (toggle color aes in geom_point)
-point_line <- ggplot(one_state, aes(x=year, y=deaths)) +
-  geom_line(aes(color=means), size=2) +
-  geom_point(aes(shape=means, color=means),  size=3)
+## See you next time!
 
