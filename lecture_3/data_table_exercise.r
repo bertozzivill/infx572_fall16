90a9a282d048ee436fd6cf8ec751ed2a82564c07
Amelia Bertozzi-Villa <abertozzivilla@intven.com>
2016-10-15 08:56:02 -0700

    Move ps1 to problem sets folder, begin work on ps2

--- lecture_3/data_table_exercise.r
+++ lecture_3/data_table_exercise.r
@@ -6,47 +6,55 @@
 library(data.table)
 
 ## Creating a data.table --------------------------------------------------------------
-
-## Create a data.table named DT that is ten elements long, with the following rows:
+library(data.table)
+## Create a data.table named DT that is ten elements long, with the following columns:
 ## V1: the integers 1-10 
 ## V2: the letters A and B, repeating
 ## V3: the integers 1-5, repeating
 ## V4: the following vector: c(4.55, 2.6, 90.9, 21.2, 4.81, 77.1, 4.4, 8.43, 5.09, 2.33)
+custom_vector <- c(4.55, 2.6, 90.9, 21.2, 4.81, 77.1, 4.4, 8.43, 5.09, 2.33)
 
+DT <- data.table(V1=1:10,
+                 V2=c("A", "B"), # rep(c("A", "B"), 5)
+                 V3=1:5,
+                 V4=custom_vector)
 
 ## Subsetting on rows (i) --------------------------------------------------------------
 
 ## Select all rows in which V3 is equal to 4
-
+DT[V3==4]
+DT[V3 %in% 4]
 
 ## Select all rows in which V3 is equal to 3 or 4
-
+DT[V3 %in% c(3,4)]
+DT[V3==3 | V3==4]
 
 ## Select all rows in which V2 is equal to A
-
+DT[V2=="A"]
 
 ## Subsetting on columns (j) --------------------------------------------------------------
 
 ## Select column V1
-
+DT[, V1]
 
 ## Select columns V1 and V4
-
+DT[, list(V1, V4)]
 
 ## Take the sum of column V4
+DT[, sum(V4)]
 
 
 ## Return the sum of column V4 (named 'sum') and the maximum value of column V1 (named 'max')
-
+DT[, list(sum=sum(V4), max=max(V1))]
 
 
 ## Doing (j) *by* group --------------------------------------------------------------
 
 ## Take the sum of column V4, by column V2
-
+DT[, sum(V4), by=V2]
 
 ## Do the same as above, but name the summed column "sum_V4"
-
+DT[, list(sum_V4=sum(V4)), by=V2]
 
 ## Do the same as above, but take the sum by column V2 and V3
 
---
+++ problem_sets/ps2.r
@@ -0,0 +1,10 @@
+##############################################################################
+## Problem Set 2: Intro Stats & Linear Regression
+## Author: Amelia Bertozzi-Villa
+## Description: Some instructions require you to write code. When I ask a 
+##              direct question please write a response in a comment.
+##
+##              Comment your code according to the guidelines discussed in 
+##              class.
+##############################################################################
+
